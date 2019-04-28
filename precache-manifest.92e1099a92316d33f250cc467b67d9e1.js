b186e861a5bf3a12275698a38e3ce600ddb2cb09
Azat <kaumov.a.r@gmail.com>
2019-04-28 22:16:50 +0300

    Updates

---
+++ precache-manifest.92e1099a92316d33f250cc467b67d9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e7259fefc3a6fcabb447fcbd8c547e",
+    "url": "/kana-go/index.html"
+  },
+  {
+    "revision": "8b3d6cad715dd8095bf8",
+    "url": "/kana-go/static/css/main.797ef7c7.chunk.css"
+  },
+  {
+    "revision": "20bdca7b6faf88c8a5cc",
+    "url": "/kana-go/static/js/2.ffe45159.chunk.js"
+  },
+  {
+    "revision": "8b3d6cad715dd8095bf8",
+    "url": "/kana-go/static/js/main.c2d60198.chunk.js"
+  },
+  {
+    "revision": "606deef35a2ffa107052",
+    "url": "/kana-go/static/js/runtime~main.30f9621d.js"
+  },
+  {
+    "revision": "870690d288f124530703b21807a410c7",
+    "url": "/kana-go/static/media/_ionicons_svg_ios-arrow-back.870690d2.svg"
+  }
+]);
