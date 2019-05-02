6ac46b0af02e8302be73a330a35487a7dc2f9633
Azat <kaumov.a.r@gmail.com>
2019-05-01 23:03:00 -0800

    Add modal component for dialogs

---
+++ src/components/modal/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import style from './style.module.scss';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.el = document.createElement('div');
+        this.el.className = style.backdrop;
+    }
+
+    componentDidMount() {
+        modalRoot.appendChild(this.el);
+    }
+
+    componentWillUnmount() {
+        modalRoot.removeChild(this.el);
+    }
+
+    render() {
+        return ReactDOM.createPortal(
+            this.props.children,
+            this.el,
+        );
+    }
+}
+
+export default Modal;
