f3e79df2e968790db25f452a3c3b8f819969db65
Azat <kaumov.a.r@gmail.com>
2019-05-04 04:22:26 -0800

    Fix character card, add content

--- src/containers/kana-table/character-card/index.js
+++ src/containers/kana-table/character-card/index.js
@@ -5,21 +5,36 @@ import Modal from "../../../components/modal";
 import style from './style.module.scss';
 
 import closeIcon from '../../../assets/icons/_ionicons_svg_md-close.svg';
+import arrowIcon from '../../../assets/icons/_ionicons_svg_ios-arrow-back.svg';
 
 class CharacterCard extends React.Component {
     render() {
-        const {onClose} = this.props;
+        const {character, onClosed} = this.props;
+        const mainSymbol = character.katakana;
         return (
             <Modal>
                 <div className={style.dialog}>
                     <button
                         className={style.closeButton}
-                        onClick={onClose}
+                        onClick={onClosed}
                     >
                         <img src={closeIcon}/>
                     </button>
                     <div className={style.content}>
-
+                        <div className={style.mainSymbol}>
+                            {mainSymbol}
+                        </div>
+                        <div className={style.romaji}>
+                            {character.romaji}
+                        </div>
+                        <div className={style.characterType}>
+                            KATAKANA
+                        </div>
+                        <div className={style.bottomInfo}>
+                            <div className={style.oneToOne}>
+                                <img src={arrowIcon} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Modal>
