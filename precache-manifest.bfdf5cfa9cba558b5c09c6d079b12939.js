49481f1872b0b527e0d02e140f85c3527e78130e
Azat <kaumov.a.r@gmail.com>
2019-05-15 20:49:16 +0300

    Updates

--- precache-manifest.bfdf5cfa9cba558b5c09c6d079b12939.js
+++ precache-manifest.bfdf5cfa9cba558b5c09c6d079b12939.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d05193425564201cc5a809095eceb94",
+    "revision": "b4ac631279065f82c0721972ffcfba5a",
     "url": "/kana-go/index.html"
   },
   {
-    "revision": "77169a8682fc4b0e3715",
-    "url": "/kana-go/static/css/main.5c18c170.chunk.css"
+    "revision": "ee0a41085ab48474b0f6",
+    "url": "/kana-go/static/css/main.7c056bd4.chunk.css"
   },
   {
-    "revision": "333aca61ae815b3ec4d0",
-    "url": "/kana-go/static/js/2.9d9a228e.chunk.js"
+    "revision": "7b7ba1fc3175f83f387b",
+    "url": "/kana-go/static/js/2.20b0af85.chunk.js"
   },
   {
-    "revision": "77169a8682fc4b0e3715",
-    "url": "/kana-go/static/js/main.d7ce0f50.chunk.js"
+    "revision": "ee0a41085ab48474b0f6",
+    "url": "/kana-go/static/js/main.bc66e529.chunk.js"
   },
   {
     "revision": "606deef35a2ffa107052",
